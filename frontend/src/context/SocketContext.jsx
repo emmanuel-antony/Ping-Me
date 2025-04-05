4ad1204a742ab5b84d1679e87c75869279c8b208
EMMANUEL  A <emmanuelantony2005@gmail.com>
2025-04-05 16:21:23 +0530

    Update SocketContext.jsx
    update url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -18,7 +18,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if(authUser){
-            const socket = io("http://localhost:5000" , {
+            const socket = io("https://ping-me-r3qm.onrender.com" , {
                 query:{
                     userId : authUser._id
                 }
