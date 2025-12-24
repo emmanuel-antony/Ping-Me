ba35f931d5ad6e153a5bcc41f4fe2136ddedbc52
25Kamalesh <kamaleshb.2004@gmail.com>
2025-12-24 15:39:18 +0530

    added duplicate field value checks
    Co-authored-by: emmanuel-antony <emmanuelantony2005@gmail.com>

--- backend/middleware/error.middleware.js
+++ backend/middleware/error.middleware.js
@@ -7,7 +7,10 @@ const ErrorHandler = (error , request , response , next ) => {
             Error.message = "Invalid Resource ID!!"
             Error.statusCode = 400
     }
-    
+    if (Error.code == 11000) {
+        Error.statusCode = 409
+        Error.message =  "Duplicate field value entered"
+    }
     response.status(Error.statusCode || 500).json({
         success: false,
         message: Error.message || "Internal Server Error😔"
