284a0da4f81defd432dde7d0449795c39daf5d1d
EMMANUEL  A <emmanuelantony2005@gmail.com>
2025-12-24 15:09:04 +0530

    Add Duplicate key error
    Added check for duplicate field value with 409 status code

--- backend/middleware/error.middleware.js
+++ backend/middleware/error.middleware.js
@@ -7,7 +7,12 @@ const ErrorHandler = (error , request , response , next ) => {
             Error.message = "Invalid Resource ID!!"
             Error.statusCode = 400
     }
-    
+    //Duplicate Key error
+    if (error.code === 11000) {
+        err.statusCode = 409
+        err.message = "Duplicate field value entered"
+    }
+
     response.status(Error.statusCode || 500).json({
         success: false,
         message: Error.message || "Internal Server Error😔"
